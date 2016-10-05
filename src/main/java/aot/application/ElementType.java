5ef749ca5a82cfa62644d4495acdfa684428b27c
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2016-10-05 05:57:00 +0300

    added log element types

--- src/main/java/aot/application/ElementType.java
+++ src/main/java/aot/application/ElementType.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016 Dmitry Kotlyarov.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package aot.application;
 
 /**
@@ -5,4 +22,18 @@ package aot.application;
  * @since 1.0
  */
 public enum ElementType {
+    TAGS((byte) -2),
+    STRING((byte) -1),
+    END((byte) 0),
+    EVENT((byte) 1),
+    BINARY_EVENT((byte) 2),
+    EXCEPTION_EVENT((byte) 3),
+    START_EVENT((byte) 4),
+    FINISH_EVENT((byte) 5);
+
+    public final byte id;
+
+    ElementType(byte id) {
+        this.id = id;
+    }
 }
